2a26781da1c5e916735f0b43675a399d334953bf
s52108 <s52108@fh.campus02.at>
2023-02-26 18:13:05 +0100

    several corrections according to the english language pt. 2

--- src/Nature.java
+++ src/Nature.java
@@ -31,10 +31,10 @@ public class Nature {
     public static void main(String[] args) {
         Nature nature = new Nature();
 
-        Cat schnurrsula = new Cat("black",2,"Schnurrsula");
-        Dog bellatrix = new Dog("black",2,"Bellatrix");
-        Frog quaxi = new Frog("green",2,"Quaxi");
-        Bird birb = new Bird("yellow",2,"birb");
+        Cat schnurrsula = new Cat("black", 2, "Schnurrsula");
+        Dog bellatrix = new Dog("black", 2, "Bellatrix");
+        Frog quaxi = new Frog("green", 2, "Quaxi");
+        Bird birb = new Bird("yellow", 2, "birb");
 
         nature.addAnimal(schnurrsula);
         nature.addAnimal(bellatrix);
@@ -46,5 +46,5 @@ public class Nature {
         System.out.println(nature.countColor("green"));
 
 
-           }
+    }
 }
