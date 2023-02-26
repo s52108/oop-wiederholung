47bf59dea346f353ddbe31302726aad444cd1c13
s52108 <s52108@fh.campus02.at>
2023-02-26 19:10:44 +0100

    Car & CarDemo - added sorting mechanism & added DemoApp

---
+++ src/org/campus02/arrays/Car.java
@@ -0,0 +1,60 @@
+package org.campus02.arrays;
+
+public class Car {
+
+    private String carType;
+    private int productionYear;
+    private int drivenKilometer;
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
+
+    public void setProdutionYear(int produtionYear) {
+        this.productionYear = produtionYear;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public int getProductionYear() {
+        return productionYear;
+    }
+
+    public int getDrivenKilometer() {
+        return drivenKilometer;
+    }
+
+    public void setDrivenKilometer(int drivenKilometer) {
+        this.drivenKilometer = drivenKilometer;
+    }
+
+    public Car(String carType, int produtionYear, int drivenKilometer) {
+        this.carType = carType;
+        this.productionYear = produtionYear;
+        this.drivenKilometer = drivenKilometer;
+    }
+
+    public static void sortByProductionYear(Car[] cars) {
+        // Sortieren des Arrays mit Hilfe der Bubble-Sort-Algorithmus
+        for (int i = 0; i < cars.length - 1; i++) {
+            for (int j = 0; j < cars.length - i - 1; j++) {
+                if (cars[j].getProductionYear() > cars[j + 1].getProductionYear()) {
+                    Car temp = cars[j];
+                    cars[j] = cars[j + 1];
+                    cars[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "carType='" + carType + '\'' +
+                ", productionYear=" + productionYear +
+                ", drivenKilometer=" + drivenKilometer +
+                '}';
+    }
+}
---
+++ src/org/campus02/arrays/CarDemo.java
@@ -0,0 +1,20 @@
+package org.campus02.arrays;
+
+import java.sql.Array;
+import java.util.Arrays;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car herbie = new Car("Cabrio", 1945, 250000);
+        Car fritzi = new Car("Coupe", 2015, 150000);
+        Car s = new Car("Model S", 2021, 25000);
+
+        Car[] cars = {fritzi,s, herbie};
+
+        System.out.println(Arrays.toString(cars));
+        Car.sortByProductionYear(cars);
+        System.out.println(Arrays.toString(cars));
+
+    }
+
+}
